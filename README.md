866d9c967272809075dbba6e2cbfe1873c90baad
Tyler Chadwick <chadwickthebold@gmail.com>
2015-04-08 00:02:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# tyc-skeleton
+
+This project is a skeleton framework for developing apps with my preferred toolkit, without having to start from scratch every time.
+
+## Current Toolchain
+
+* Gulp
+* Webpack
+* Backbone
+* React
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    del = require('del'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    debug = require('gulp-debug'),
+    jshint = require('gulp-jshint'),
+    less = require('gulp-less'),
+    mocha = require('gulp-mocha'),
+    react = require('gulp-react'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    webpack = require('webpack');
+
+gulp.task('default', []);
+
